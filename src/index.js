6e10697ea9a8ba0738b76fc8964c7fa6e997bbf7
hypo <you@example.com>
2018-05-15 23:38:41 +0430

    remove dummy expense

--- src/index.js
+++ src/index.js
@@ -21,13 +21,13 @@ import './index.css';
 import 'react-dates/initialize'
 const store = configureStore()
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))
-store.dispatch(addExpense({ description: 'Gas bill' , createdAt : 1000 }))
-store.dispatch(addExpense({ description: 'rent', amount: 105081 }))
+// store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))
+// store.dispatch(addExpense({ description: 'Gas bill' , createdAt : 1000 }))
+// store.dispatch(addExpense({ description: 'rent', amount: 105081 }))
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
